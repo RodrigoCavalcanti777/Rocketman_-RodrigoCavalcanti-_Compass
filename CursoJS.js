6d0c2c7d30d7edcd1540c57e71b0f11bfa418905
Rodrigo Silva <rodrigosilvester123@gmail.com>
2022-05-13 13:31:39 -0300

    Arquivos repassados para o novo repositório

---
+++ CursoJS.js
@@ -0,0 +1,76 @@
+/*Váriaveis
+
+let idade = 5;
+console.log(idade);
+let tamanho = 1.70;
+console.log(tamanho);
+*/
+
+/*Constantes
+const valorIngresso = 30;
+valorIngresso = 40;
+console.log(valorIngresso)
+*/
+
+/*Tipos primitivos
+
+let nome = 'Rodrigo'; //String literal
+let idade = 20;// number literal
+let estaAprovado = true; // boolean
+let sobreNome; // valor Padrão ou Undefinied
+let corSelecionada = null // usada para reset ou redefinir um valor
+*/
+
+/*Tipagem Dinâmica
+
+O Javascript é uma linguagem dinamica e inteligente, 
+reconhecendo o tipo da váriavel.
+
+*/
+
+// Objetos
+
+let pessoa = {
+    nome: 'Rodrigo',
+    sobrenome: 'Silva'
+}
+console.log(pessoa);
+
+//Arrays
+
+let formula1 = ['Senna', 2, 1, 3];
+console.log(formula1);
+
+//functions
+function Dobro(numero){
+return numero * 2;
+}
+
+console.log(Dobro(5));
+
+
+/* Operador ternário
+
+O operador ternário serve por exemplo, para não usar tantos if e elses..
+
+*/
+//cliente com o título de comum e premium
+let pontos = 101;
+let tipo = pontos > 100 ? 'premium' : 'comum'
+
+console.log(tipo);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,22 @@
+# GIT E GITHUB
+
+#INFORMAÇÕES: 
+
+- Olá meu nome é Rodrigo Cavalcanti e esse é um projeto onde vai ser 
+- armazenadas todas as minhas atividades diárias na compass.uol.
+
+-Arquivos:
+
+-CursoJs = Arquivo com os assuntos passados no curso de Javascript.
+
+
+#author: Rodrigo Cavalcanti da silva
+
+
+referências:
+-https://www.youtube.com/watch?v=i6Oi-YtXnAU&t=497s
+
+
+
+
+##Compass.uol
