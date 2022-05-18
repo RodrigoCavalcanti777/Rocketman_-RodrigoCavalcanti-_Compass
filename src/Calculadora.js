ab12d9888decf2714128f24ed30261fc2a6a1c7e
Rodrigo Silva <rodrigosilvester123@gmail.com>
2022-05-18 12:10:51 -0300

    Atualizações do Projeto TDD

---
+++ src/Calculadora.js
@@ -0,0 +1,22 @@
+
+
+export default class Calculadora {
+
+static soma(a,b){
+    return a + b;
+}
+
+static sub(a,b){
+    return a - b;
+}
+
+static multi(a,b){
+    return a * b;
+}
+
+static div(a,b){
+    return a /b;
+}
+
+
+}
--- test/Calculadora.spec.js
+++ test/Calculadora.spec.js
@@ -1,11 +1,19 @@
 import assert from 'assert'
 import chai from 'chai';
+import Calculadora from '../src/Calculadora.js';
 
 const expect = chai.expect;
 
 
+describe('Verificar Soma', () => {
+    it.only('Soma de a+b com solução de ab', () => {
+        let resultado = Calculadora.soma(2, 2)
+        expect(resultado).to.be.eq(4);
+    })
+})
+
 describe('meu primeiro teste', () => {
-    it.only('Verificar a Váriavel Auxiliar', () => {
+    it('Verificar a Váriavel Auxiliar', () => {
         let aux = 2;
         // assert.strictEqual(aux, 2)
         expect(aux).to.be.equals(2);
@@ -18,6 +26,7 @@ describe('meu primeiro teste', () => {
     })
 
 })
+
 describe('Meu Segundo teste', () => {
     it('Verificar Desigualdade', () => {
         let aux = 2
